f842cb2a071dd285d6fd406d740558655c799e62
Ralph Delia <Ralphdelia1@gmail.com>
2025-12-05 09:40:18 -0500

    test prod db working

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-// Disable prefetch as it's not supported for "Transaction" pool mode
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+const client = postgres(process.env.DATABASE_URL!, {
+  prepare: false,
+  max: 1,
+});
 export const db = drizzle(client, { schema });

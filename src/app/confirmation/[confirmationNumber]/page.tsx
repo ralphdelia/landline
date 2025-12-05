9beb0b0fcd649656c3951bd4396ae35073d54904
Ralph Delia <Ralphdelia1@gmail.com>
2025-12-05 18:12:26 -0500

    update confrimation page styles and await params

--- src/app/confirmation/[confirmationNumber]/page.tsx
+++ src/app/confirmation/[confirmationNumber]/page.tsx
@@ -1,25 +1,23 @@
-export default function ConfirmationPage({
+export default async function ConfirmationPage({
   params,
 }: {
   params: { confirmationNumber: string };
 }) {
+  const { confirmationNumber } = await params;
   return (
-    <div className="bg-background flex min-h-screen items-center justify-center font-sans">
-      <main className="bg-background flex min-h-screen w-full max-w-3xl flex-col items-center p-8 sm:items-start">
-        <h1 className="mb-4 text-2xl font-bold">Booking Confirmed</h1>
-        <p className="mb-6 text-sm text-gray-500">
-          Confirmation Number: {params.confirmationNumber}
-        </p>
+    <div className="">
+      <h1 className="mb-4 text-2xl font-bold">Booking Confirmed</h1>
+      <p className="mb-6 text-sm text-stone-800">
+        Confirmation Number: {confirmationNumber}
+      </p>
 
-        <section className="w-full">
-          <h2 className="mb-3 text-xl font-semibold">Thank You</h2>
-          <p className="mb-4">Your booking has been confirmed successfully.</p>
-          <p className="text-sm text-gray-600">
-            You will receive a confirmation email shortly with your trip
-            details.
-          </p>
-        </section>
-      </main>
+      <section className="w-full">
+        <h2 className="mb-3 text-xl font-semibold">Thank You</h2>
+        <p className="mb-4">Your booking has been confirmed successfully.</p>
+        <p className="text-sm text-gray-600">
+          You will receive a confirmation email shortly with your trip details.
+        </p>
+      </section>
     </div>
   );
 }

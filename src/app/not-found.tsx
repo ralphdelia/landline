54354bbad7601eace654ea118d2a91663e87bbc9
Ralph Delia <Ralphdelia1@gmail.com>
2025-12-06 15:12:33 -0500

    update not found styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="bg-background flex min-h-screen items-center justify-center font-sans">
-      <main className="flex flex-col items-center justify-center text-center p-8">
-        <h1 className="text-6xl font-bold mb-4">404</h1>
-        <h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
-        <p className="text-gray-600 mb-8">
+    <div className="flex items-center justify-center font-sans">
+      <section className="flex flex-col items-center justify-center p-8 text-center">
+        <h1 className="mb-4 text-6xl font-bold">404</h1>
+        <h2 className="mb-4 text-2xl font-semibold">Page Not Found</h2>
+        <p className="mb-8 text-gray-600">
           The page you are looking for does not exist.
         </p>
-        <Link 
-          href="/" 
-          className="text-blue-600 hover:underline font-medium"
+        <Link
+          href="/"
+          className="font-medium text-blue-600 hover:cursor-pointer hover:underline"
         >
           Return to Home
         </Link>
-      </main>
+      </section>
     </div>
   );
 }
